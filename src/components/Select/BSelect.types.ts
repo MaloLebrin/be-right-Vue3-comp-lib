02ac4fc75e80bef08313f7fc8a7ff3cb896db182
malolebrin <malolebrin@gmail.com>
2021-12-03 17:57:51 +0100

    FIX sleect colors

--- src/components/Select/BSelect.types.ts
+++ src/components/Select/BSelect.types.ts
@@ -12,7 +12,7 @@ export type SelectColorsMap = {
 	[key in SelectColorsEnum]: string
 }
 
-const commonClasses = 'text-gray-900 select-none relative py-2 pl-3 pr-9  cursor-pointer hover:text-white focus:outline-none focus:text-white'
+const commonClasses = 'text-gray-900 select-none relative py-2 pl-3 pr-9  cursor-pointer hover:text-white focus:outline-none focus:text-white text-black dark:text-white'
 
 export const SelectColorsMap: SelectColorsMap = {
 	primary: `${commonClasses} hover:bg-blue focus:bg-blue`,
